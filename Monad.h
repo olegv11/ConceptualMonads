e94b24b848538ef05bdcf4d3bdfa126d284559c0
olegv11 <olegsik@gmail.com>
2018-01-29 16:04:53 +0300

    Added comments

--- Monad.h
+++ Monad.h
@@ -12,40 +12,59 @@ template <typename F, typename ...Args>
 concept bool Callable =
     std::is_invocable<F, Args...>::value;
 
+// A class which, when specialized on M, should contain the definitions
+// for pure and bind of a monad M.
 template <template <typename, typename...> typename M, typename T, typename ...Args>
 struct MonadDetails;
 
+// Dummy classes that represent "any" type
 struct Dummy{};
 struct Dummy2{};
 
+// pure/return :: a -> m a
+// Something is PurableTo<M,T,Args...> if there exists a function "pure"
+// that takes in T and produces an M<T,Args...>
 template <template <typename, typename...> typename M, typename T, typename ...Args>
-concept bool PurableTo = requires(T t) {
+concept bool PurableTo = requires(T t)
+{
     { MonadDetails<M,T,Args...>::pure(t) } -> M<T, Args...>;
 };
 
+// M is Purable if we can construct M A from any A.
+// As C++ can't really express a concept of "any A", we use a Dummy class.
 template <template <typename, typename...> typename M, typename ...Args>
 concept bool Purable = PurableTo<M,Dummy, Args...>;
 
+
+// T -> M Dummy
 template<template <typename, typename...> typename M, typename T, typename ...Args>
-M<Args..., Dummy> ToDummy(T)
+M<Dummy, Args...> ToDummy(T)
 {
     return MonadDetails<M,Dummy,Args...>::pure(Dummy{});
 }
 
-
+// bind/>>= :: m a -> (a -> m b) -> m b
+// Something is BindableTo<M,T,O,F,Args...> if there exists a function bind
+// that takes in M<T, Args...> and a function F and produces an M<O>
 template <template <typename, typename...> typename M, typename T, typename O, typename F, typename ...Args>
 concept bool BindableTo = requires(M<T, Args...> m, F f)
 {
-        { MonadDetails<M,T,Args...>::template bind<O,F>(m,f) };
+        { MonadDetails<M,T,Args...>::template bind<O,F>(m,f) } -> M<O>;
 };
 
+// M is Bindable if for any A and B (here Dummy2 and Dummy) and any
+// function A -> M B there is a function "bind"
 template <template <typename, typename...> typename M, typename ...Args>
 concept bool Bindable = BindableTo<M, Dummy2, Dummy, decltype(ToDummy<M, Dummy2>), Args...>;
 
-
+// A monad M is something that is Purable and Bindable. That is, in Haskell:
+// class Monad m where
+//   (>>=)  :: m a -> (  a -> m b) -> m b
+//   return ::   a                 -> m a
 template<template <typename, typename...> typename M, typename ...Args>
 concept bool Monad = Purable<M, Args...> && Bindable<M, Args...>;
 
+// Default implementation of >>=
 template <template <typename, typename...> typename M, typename T, typename F, typename ...Args>
 requires Callable<F, T>
 auto operator>>=(M<T, Args...> m, F f) -> typename std::invoke_result<F,T>::type
@@ -55,14 +74,14 @@ auto operator>>=(M<T, Args...> m, F f) -> typename std::invoke_result<F,T>::type
     return Details::template bind<Res, F>(m, f);
 }
 
-// It's not a replacement for a do-notation, but it'll do.
+// Something that resembles do-notation
 template <typename ...Args>
 auto bindAll(Args... args)
 {
     return (... >>= args);
 }
 
-
+// Default implementation of fmap
 template<Monad M, typename T, typename F, typename ...Args>
 requires Callable<F, T>
 auto fmap(F f, M<T, Args...> m) -> M<typename std::invoke_result_t<F,T>>
